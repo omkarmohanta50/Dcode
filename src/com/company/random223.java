9fb0218b709ab74ecf94a686f8cba2a63c34c195
omkar mohanta <omkarmohanta5060@gmail.com>
2023-08-13 16:52:10 +0530

    random223 file added

---
+++ src/com/company/random223.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class random223 {
+    public static void main(String[] args) {
+        System.out.println("sabya is handsome and the hottest guy in our area");
+    }
+}
